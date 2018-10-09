e14ac78de2d511bbd09f6e929057a0c122f9125d
cyriakleclercq <cyriakleclercq@gmail.com>
2018-10-09 17:00:31 +0200

    test1

---
+++ cv.js
@@ -0,0 +1,32 @@
+function bouton1() {
+
+
+    var x = document.getElementById('test');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function bouton2() {
+
+
+    var x = document.getElementById('compétences');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function bouton3() {
+
+
+    var x = document.getElementById('test2');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
